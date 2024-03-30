e6c3fc44427be419d62c820fca28633ca71e7fa1
Hariharan <hariharan15060@gmail.com>
2024-03-30 22:35:24 +0530

    eCommerce

---
+++ js/Key.js
@@ -0,0 +1,39 @@
+// !!!! ONLY FOR REFERENCE !!!!
+
+// VARIABLE DECLARATION
+{
+    var VARIABLE_NAME = document.getElementById('ID').Value;
+}
+// PRINTING
+{
+    console.log("COMMAND",ATTRIBUTE);
+}
+// ARRAY
+{
+    var ARRAY = ['VAL1',122,true];
+}
+// OBJECTS
+{
+    var OBJECTS = {
+        KEY1 : "VALUE1",
+        KEY2 : 22,
+        KEY3 : false
+    }
+}
+// ARRAY OF OBJECTS
+{
+    var ARRAY_OF_OBJECTS = [
+        {KEY1 : "VALUE1",KEY2 : 11,KEY3 : true},
+        {KEY1 : "VALUE2",KEY2 : 22,KEY3 : false},
+        {KEY1 : "VALUE3",KEY2 : 33,KEY3 : false}
+    ]
+}
+// IF ELSE
+{
+    if(Condition){
+        console.log(STATEMENT_1);
+    } else {
+        console.log(STATEMENT_2);
+    }
+}
+// 
